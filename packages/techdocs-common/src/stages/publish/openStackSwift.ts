7125496637bba297225e0f393c00878073cc9e24
Eric Peterson <ericpeterson@spotify.com>
2021-11-12 10:43:05 +0100

    Update all Publishers so that they resolve a list of objects on successful publish.
    Signed-off-by: Eric Peterson <ericpeterson@spotify.com>

--- packages/techdocs-common/src/stages/publish/openStackSwift.ts
+++ packages/techdocs-common/src/stages/publish/openStackSwift.ts
@@ -28,6 +28,7 @@ import { getFileTreeRecursively, getHeadersForFileExtension } from './helpers';
 import {
   PublisherBase,
   PublishRequest,
+  PublishResponse,
   ReadinessResponse,
   TechDocsMetadata,
 } from './types';
@@ -135,8 +136,13 @@ export class OpenStackSwiftPublish implements PublisherBase {
    * Upload all the files from the generated `directory` to the OpenStack Swift container.
    * Directory structure used in the bucket is - entityNamespace/entityKind/entityName/index.html
    */
-  async publish({ entity, directory }: PublishRequest): Promise<void> {
+  async publish({
+    entity,
+    directory,
+  }: PublishRequest): Promise<PublishResponse> {
     try {
+      const objects: string[] = [];
+
       // Note: OpenStack Swift manages creation of parent directories if they do not exist.
       // So collecting path of only the files is good enough.
       const allFilesToUpload = await getFileTreeRecursively(directory);
@@ -157,6 +163,7 @@ export class OpenStackSwiftPublish implements PublisherBase {
         // The / delimiter is intentional since it represents the cloud storage and not the local file system.
         const entityRootDir = `${entity.metadata.namespace}/${entity.kind}/${entity.metadata.name}`;
         const destination = `${entityRootDir}/${relativeFilePathPosix}`; // Swift container file relative path
+        objects.push(destination);
 
         // Rate limit the concurrent execution of file uploads to batches of 10 (per publish)
         const uploadFile = limiter(async () => {
@@ -174,7 +181,7 @@ export class OpenStackSwiftPublish implements PublisherBase {
       this.logger.info(
         `Successfully uploaded all the generated files for Entity ${entity.metadata.name}. Total number of files: ${allFilesToUpload.length}`,
       );
-      return;
+      return { objects };
     } catch (e) {
       const errorMessage = `Unable to upload file(s) to OpenStack Swift. ${e}`;
       this.logger.error(errorMessage);
